f82317b278e0cd91d8fe053e723584980bbfee6f
unique258 <Jtq666666@gmail.com>
2023-12-26 19:50:29 +0800

    新增路径

--- controller/lujing.py
+++ controller/lujing.py
@@ -1,22 +1,22 @@
-from sqlml import Sql  # 导入自定义模块sqlML中的类Sql
+from sql import Sql
 import math
 
 
-# chengshi=[]       # 城市 (chengshi) 列表来保存路径
-
+sql =Sql()
+sql.connect()
 
 def lujing2(chufa, mudi, chengshi):  # 输入出发与到达市,通过递归  贪心算法  计算出最短路径   只供 lujing()调用
-    sqlm = "select 省会城市 from adjacency where " + chufa + "=1"  # 编辑sql命令
-    xlcs = Sql.sql1(sqlm)
+    statement = f"select 省会城市 from adjacency where {chufa}=1"  # 编辑sql命令
+    xlcs = sql.execute_query(statement)
     # print(chufa,'-',mudi,"附近的城市",xlcs)
-    sqlm = "select longitude,latitude from city where city_name ='" + mudi + "'"
-    jwd0 = Sql.sql1(sqlm)  # 目的地经纬度
+    statement = f"select longitude,latitude from city where city_name ='{mudi}'"
+    jwd0 = sql.execute_query(statement) # 目的地经纬度
 
     juli = 0  # 初始化两城市之间距离,算法为经度纬度的差值
     chengshi2 = "0"  # 初始化出发城市最近的城市.用来保存最近城市的变量
     for x in xlcs:  # 遍历附近的城市                   #获取附近城市经纬度,比较哪个离目的地最近,如果是目的城市,直接跳出
-        sqlm = "select longitude,latitude from city where city_name ='" + x[0] + "'"
-        jwd = Sql.sql1(sqlm)
+        statement = f"select longitude,latitude from city where city_name ='{x[0]}'"
+        jwd = sql.execute_query(statement)
         a = float(jwd[0][0]) - float(jwd0[0][0])
         b = float(jwd[0][1]) - float(jwd0[0][1])
         juli2 = math.sqrt(pow(a, 2) + pow(b, 2))
@@ -42,11 +42,11 @@ def lujing(chufa, mudi):  # 函数功能是输入2省 计算出最优路径
     if chufa == mudi:  # 如果是本省快递,返回0
         return ('0',)
     else:  # 不是本省快递,调用lujing2计算最优路径
-        sqlm = "select capital_city from province where prv_name='" + chufa + "'"
-        shcs = Sql.sql1(sqlm)  # shcs 省会城市
+        statement = f"select capital_city from province where prv_name='{chufa}'"
+        shcs = sql.execute_query(statement) # shcs 省会城市
 
-        sqlm = "select capital_city from province where prv_name='" + mudi + "'"
-        shcs2 = Sql.sql1(sqlm)  # shcs 省会城市
+        statement = f"select capital_city from province where prv_name='{mudi}'"
+        shcs2 = sql.execute_query(statement)  # shcs 省会城市
 
         chengshi = shcs[0][0],  # 将出发城市添加到路径表的第一位
         chengshi = lujing2(shcs[0][0], shcs2[0][0], chengshi)  # 调用函数 计算最短路径列表
